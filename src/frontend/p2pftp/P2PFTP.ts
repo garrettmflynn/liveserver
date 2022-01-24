2957b0841e9d880a2256bcad83d62d8ce25cb356
Bubba Skrimp <brewster.joshua1@gmail.com>
2022-01-24 01:05:59 -0800

    wip

--- src/frontend/p2pftp/P2PFTP.ts
+++ src/frontend/p2pftp/P2PFTP.ts
@@ -1,11 +1,73 @@
-//import * from 'brainsatplay-storage'
+import * as BFS from 'brainsatplay-storage'
+
+type BufferedChunk = {
+    filename:string,
+    source:string,
+    size:number,
+    start:number|undefined, //let us know what chunk of the file we have
+    end:string|number|undefined,
+};
+
+//for seeding or leeching
+type FTPSource = {
+    filename:string,
+    source:string,
+    size:number,
+    start:number|undefined, //let us know what chunk of the file we have
+    end:string|number|undefined,
+    buffered:BufferedChunk[] //lets us know if we have multiple pieces of a file
+}
+
+
 
 //Uses a BrowserFS instance of IndexedDB to create a file transfer protocol, e.g. through WebRTC (like Webtorrent)
+//Webtorrent will seed files sequentially to buffer e.g. video, or can seed randomly as normal torrents. We should do both too
 export class P2PFTP {
+    files:string[];
+    leeching:FTPSource[];
+    seeding:FTPSource[];
 
-    constructor() {
+    constructor (leeching:FTPSource[], seeding:FTPSource[], localdir='data') {
+        this.seeding = seeding; //filenames 
+        this.leeching = leeching;
+
+        this.setupFTP(localdir);
 
     }
+
+
+    async setupFTP(localdir:string) {
+        this.files = await BFS.listFiles(localdir);
+
+        this.seeding.forEach((f:FTPSource) => {
+            this.seedIndexedDBFile(f.filename,f.source);
+        });
+
+        this.leeching.forEach((f:FTPSource) => {
+            this.leechFile(f.filename,f.source,f.start,f.end,false);
+        })
+    }
+
+    
+    //seed a local file
+    seedIndexedDBFile(filename:string, source:string|undefined) {
+        //create a room or supply a source to seed through
+    }
+
+    //if toCSV is false, writes leeched file to indexedDB by default. Can leech only chunks of a file
+    //Need to know the size of the file and preallocate if we can or otherwise keep the data in order if its seeded randomly
+    leechFile(filename:string, source:string, start:number=0, end:string|number='end', toCSV=false) {
+        //connect to the source pool and ask for data
+    }
+
+    //As you write to IndexedDB, make it available for seeding
+    reseedFile(filename:string, source:string|undefined) {
+        //check if its already being leeched or exists locally to be seeded
+    }   
+
+    //then more BrowserFS macros
+
+    //also create parity on the backend
 
 }
 
@@ -28,5 +90,13 @@ If a peer wants to seed, stream the chunks to them and they become a seeder. Can
     if distributing chunks of the dataset, some chunks may go offline but the data will be more widely distributable
 
 We can use the Session service to organize rooms with access and customizable controls
+
+
+
+Peer --> Request file chunk
+Seeder --> Parse file chunk from DB, stream in chunks
+Peer --> Receive chunks, parse into IndexedDB to become seeder or for downloading/backing up
+
+
 
 */
